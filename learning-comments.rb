07b7ded8d560700e0a98a50f077392d2d6498e12
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-09-14 12:56:09 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,25 @@
+# Perform a constant expression evaluation
+3
+# Assign constant 3 to bare-word variable triangle_sides
+triangle_sides = 3
+
+
+array_of_arrays = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+count = 0
+ 
+while count < array_of_arrays.length do
+  puts array_of_arrays[count]
+  count += 1
+end
+
+tests = {
+  test1: ["result1", "result2", "result3"],
+  test2: ["result1", "result2", "result3"],
+  test3: ["result1", "result2", "result3"],
+  test4: ["result1", "result2", "result3"]
+}
